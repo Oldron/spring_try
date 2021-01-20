57195c57c26744e57d3e602fb4d444183d35f50b
Oldron <oldron@yandex.ru>
2021-01-20 14:31:57 +0300

    add swagger

---
+++ src/main/java/com/example/spring_try/spring/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.spring_try.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan("com.example.spring_try")
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
